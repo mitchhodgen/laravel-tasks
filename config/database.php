99c0b4a1a6e07d064805ba4fe4766bfe9d0d22f6
mitchhodgen <131394612+mitchhodgen@users.noreply.github.com>
2023-09-29 09:41:34 -0400

    Config DB & Redis variables

--- config/database.php
+++ config/database.php
@@ -135,12 +135,12 @@ return [
         ],
 
         'cache' => [
-            'url' => env('REDIS_URL'),
+            'scheme' => 'tls',
             'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
+            'username' => env('REDIS_USERNAME'),
             'password' => env('AZURE_REDIS_PASSWORD', null),
             'port' => env('AZURE_REDIS_PORT', '6379'),
             'database' => env('AZURE_REDIS_DB', '1'),
-            'scheme' => 'tls',
         ],
 
     ],
